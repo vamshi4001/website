6842ba84b15037adb3a9411029ee628e69085f9d
Vamshi <vamshi4001@gmail.com>
2017-07-08 15:00:47 -0400

    Added text box to get required quantity

--- controllers/homeController.js
+++ controllers/homeController.js
@@ -6,8 +6,13 @@ angular.module("firstApp")
 		$scope.cryptos = {"BTC": {"id": "bitcoin", "name": "Bitcoin", "symbol": "BTC", }, "ETH": {"id": "ethereum", "name": "Ethereum", "symbol": "ETH", }, "XRP": {"id": "ripple", "name": "Ripple", "symbol": "XRP", }, "LTC": {"id": "litecoin", "name": "Litecoin", "symbol": "LTC", }, "DASH": {"id": "dash", "name": "Dash", "symbol": "DASH", }, "ETC": {"id": "ethereum-classic", "name": "Ethereum Classic", "symbol": "ETC", }, "XEM": {"id": "nem", "name": "NEM", "symbol": "XEM", }, "XMR": {"id": "monero", "name": "Monero", "symbol": "XMR", }, "GNT": {"id": "golem-network-tokens", "name": "Golem", "symbol": "GNT", }, "REP": {"id": "augur", "name": "Augur", "symbol": "REP", }, "MAID": {"id": "maidsafecoin", "name": "MaidSafeCoin", "symbol": "MAID", }, "ZEC": {"id": "zcash", "name": "Zcash", "symbol": "ZEC", }, "GNO": {"id": "gnosis-gno", "name": "Gnosis", "symbol": "GNO", }, "PIVX": {"id": "pivx", "name": "PIVX", "symbol": "PIVX", }, "STRAT": {"id": "stratis", "name": "Stratis", "symbol": "STRAT", }, "DOGE": {"id": "dogecoin", "name": "Dogecoin", "symbol": "DOGE", }, "DCR": {"id": "decred", "name": "Decred", "symbol": "DCR", }, "FCT": {"id": "factom", "name": "Factom", "symbol": "FCT", }, "STEEM": {"id": "steem", "name": "Steem", "symbol": "STEEM", }, "DGD": {"id": "digixdao", "name": "DigixDAO", "symbol": "DGD", }, "WAVES": {"id": "waves", "name": "Waves", "symbol": "WAVES", }, "USDT": {"id": "tether", "name": "Tether", "symbol": "USDT", }};
 
 		$scope.url = "./coinlist.json";
+		$scope.completeTotal = 0;
+		$scope.coinlist = [];
+		$scope.$watch($scope.coinlist, function(o, n){
+			console.log(o);
+			console.log(n);
+		})
 		$http.get($scope.url).then(function(response){
-			$scope.coinlist = [];
 			if(response.data.Response=="Success"){
 				$scope.BaseImageUrl = response.data.BaseImageUrl;
 				$scope.BaseLinkUrl = response.data.BaseLinkUrl;
@@ -18,7 +23,8 @@ angular.module("firstApp")
 				}
 				$scope.coinlist.sort(function(a, b) {
 				    return parseInt(a.SortOrder) - parseInt(b.SortOrder);
-				});
+				});				
+				
 				// $scope.coinlist = $scope.coinlist.slice(0,98);
 			}
 			var keys = Object.keys($scope.cryptos);
