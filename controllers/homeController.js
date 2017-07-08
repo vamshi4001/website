15af3099afefa2a5393a1ff6c2eb21e1168b0ccb
Vamshi <vamshi4001@gmail.com>
2017-07-08 16:05:03 -0400

    Quantity and Total calculation added

--- controllers/homeController.js
+++ controllers/homeController.js
@@ -6,12 +6,21 @@ angular.module("firstApp")
 		$scope.cryptos = {"BTC": {"id": "bitcoin", "name": "Bitcoin", "symbol": "BTC", }, "ETH": {"id": "ethereum", "name": "Ethereum", "symbol": "ETH", }, "XRP": {"id": "ripple", "name": "Ripple", "symbol": "XRP", }, "LTC": {"id": "litecoin", "name": "Litecoin", "symbol": "LTC", }, "DASH": {"id": "dash", "name": "Dash", "symbol": "DASH", }, "ETC": {"id": "ethereum-classic", "name": "Ethereum Classic", "symbol": "ETC", }, "XEM": {"id": "nem", "name": "NEM", "symbol": "XEM", }, "XMR": {"id": "monero", "name": "Monero", "symbol": "XMR", }, "GNT": {"id": "golem-network-tokens", "name": "Golem", "symbol": "GNT", }, "REP": {"id": "augur", "name": "Augur", "symbol": "REP", }, "MAID": {"id": "maidsafecoin", "name": "MaidSafeCoin", "symbol": "MAID", }, "ZEC": {"id": "zcash", "name": "Zcash", "symbol": "ZEC", }, "GNO": {"id": "gnosis-gno", "name": "Gnosis", "symbol": "GNO", }, "PIVX": {"id": "pivx", "name": "PIVX", "symbol": "PIVX", }, "STRAT": {"id": "stratis", "name": "Stratis", "symbol": "STRAT", }, "DOGE": {"id": "dogecoin", "name": "Dogecoin", "symbol": "DOGE", }, "DCR": {"id": "decred", "name": "Decred", "symbol": "DCR", }, "FCT": {"id": "factom", "name": "Factom", "symbol": "FCT", }, "STEEM": {"id": "steem", "name": "Steem", "symbol": "STEEM", }, "DGD": {"id": "digixdao", "name": "DigixDAO", "symbol": "DGD", }, "WAVES": {"id": "waves", "name": "Waves", "symbol": "WAVES", }, "USDT": {"id": "tether", "name": "Tether", "symbol": "USDT", }};
 
 		$scope.url = "./coinlist.json";
-		$scope.completeTotal = 0;
 		$scope.coinlist = [];
-		$scope.$watch($scope.coinlist, function(o, n){
-			console.log(o);
-			console.log(n);
-		})
+		$scope.completeTotalUSD = 0;
+		$scope.completeTotalINR = 0
+			
+		$scope.updateCost = function(){
+			$scope.completeTotalUSD = 0;
+			$scope.completeTotalINR = 0
+			for(i in $scope.coinlist){
+				if($scope.coinlist[i] && $scope.prices[$scope.coinlist[i].Name]){
+					$scope.completeTotalUSD += Math.round(parseFloat($scope.coinlist[i].quantity?$scope.coinlist[i].quantity:0)*$scope.prices[$scope.coinlist[i].Name]["USD"]*100)/100;
+					$scope.completeTotalINR += Math.round(parseFloat($scope.coinlist[i].quantity?$scope.coinlist[i].quantity:0)*$scope.prices[$scope.coinlist[i].Name]["USD"]*$scope.conversion["INR"]*100)/100;
+
+				}				
+			}
+		}
 		$http.get($scope.url).then(function(response){
 			if(response.data.Response=="Success"){
 				$scope.BaseImageUrl = response.data.BaseImageUrl;
@@ -34,16 +43,7 @@ angular.module("firstApp")
 					$scope.conversion = res.data.rates;
 				})
 			})			
-		})
-		function compare(a,b) {
-		  if (a.last_nom < b.last_nom)
-		    return -1;
-		  if (a.last_nom > b.last_nom)
-		    return 1;
-		  return 0;
-		}
-
-		
+		})		
 	}
 
 	// Status - 200, 404, 500, 403, 301
